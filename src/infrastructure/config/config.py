53d3bc3f4593eff5d9f28dae57cb00d4f7471dd7
matixezor <mateusz_romanski@interia.pl>
2022-11-20 18:58:32 +0100

    feat: add recommender [ALKL-77]

--- src/infrastructure/config/config.py
+++ src/infrastructure/config/config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-ALLOWED_ORIGINS = [getenv('ALLOWED_ORIGINS')]
+ALLOWED_ORIGINS = [getenv('ALLOWED_ORIGINS', '*')]
 ALLOWED_METHODS = ['GET']
 ALLOWED_HEADERS = ['*']
 ALLOW_CREDENTIALS = False
